a0e639468e07a4c49c8889daa4aa52b0ad35eace
Aizhu Xue <aizhudeveloper@gmail.com>
2024-01-31 06:54:26 -0500

    explained better

--- fireship6.js
+++ fireship6.js
@@ -2,7 +2,7 @@ const tick = Date.now()
 const log = (v) => console.log(`${v} \n Elapsed time ${Date.now() - tick}`)
 
 const codeBlocker = () => {
-    // instead of creating promise, resolve it to put it in microtask
+    // instead of creating promise, resolve so we can have this code run after the sync code and macrotasks have been completed
     return Promise.resolve().then(v => {
         let i = 0;
         while(i < 1000000000) { i++ }
